3171f8205691404ca067c146b463d5a5cb793781
Eirik Jensen <eiriktj@pm.me>
2020-08-09 18:40:54 +0200

    Add installation requirements and test requirements.

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
+
 setup(
     name="Ligabot",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'lxml'
+    ],
+    tests_require=['pytest'],
 )
